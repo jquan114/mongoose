8fe686f190ce9baf2276d9fde92a6dc32b79f9e4
JQUAN CALEK <jquancollins96@gmail.com>
2022-05-24 20:48:11 -0400

    Need more structure on routes and my other pages.

--- server.js
+++ server.js
@@ -1,82 +1,93 @@
-
-require('dotenv').config();
+require("dotenv").config();
 
 // Dependencies
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const PORT = 3000;
-const methodOverride = require('method-override')
-const Product = require('./models/products');
-const productSeed = require('./models/productSeed');
+const methodOverride = require("method-override");
+const Product = require("./models/products");
+const productSeed = require("./models/productSeed");
 const db = mongoose.connection;
 
 // Database Configuration
 const DATABASE_URL =
-	'mongodb+srv://admin:abc1234@cluster0.aftg6.mongodb.net/mongoStore?retryWrites=true&w=majority';
+  "mongodb+srv://admin:abc1234@cluster0.aftg6.mongodb.net/mongoStore?retryWrites=true&w=majority";
 // Database Connection
 mongoose.connect(DATABASE_URL, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 // Database Connection Error/Success - optional but can be really helpful
-db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
-db.on('connected', () => console.log('mongo connected'));
-db.on('disconnected', () => console.log('mongo disconnected'));
-
+db.on("error", (err) => console.log(err.message + " is Mongod not running?"));
+db.on("connected", () => console.log("mongo connected"));
+db.on("disconnected", () => console.log("mongo disconnected"));
 
 // Middleware
 // Body parser middleware: it creates req.body
 app.use(express.urlencoded({ extended: false }));
-app.use(methodOverride("_method"))
+app.use(methodOverride("_method"));
 // app.use(morgan('dev'))
 
-//<--------------ROUTES------------> 
+//<--------------ROUTES------------>
 
-// Create
-app.get('/products', (req, res) => {
-    Product.find({},(error,allProducts) => {
-        res.render('index.ejs', {products: allProducts})
-    })
+// Create //
+app.get("/products", (req, res) => {
+  Product.find({}, (error, allProducts) => {
+    res.render("index.ejs", { products: allProducts });
+  });
 });
 
-//
-app.get('/products/seed', (req, res) => {
- 
-    Product.deleteMany({}, (error, allProducts) => {});
+// Product Seed Route //
+app.get("/products/seed", (req, res) => {
+  Product.deleteMany({}, (error, allProducts) => {});
 
-    Product.create(productSeed,
+  Product.create(
+    productSeed,
 
-        
-        (error, data) => {
-            res.redirect('/products');
-        }
-    );
+    (error, data) => {
+      res.redirect("/products");
+    }
+  );
 });
-//Read
+// Read //
 
+// New //
+app.get("/products/new", (req, res) => {
+  res.render("new.ejs",);
+});
 
-//New
-app.get('/products/new', (req, res) => {
-    res.render('new.ejs')
-})
+// Show //
+app.get("/products/:id", (req, res) => {
+  Product.findById(req.params.id, (err, foundProduct) => {
+    res.render("show.ejs", {
+      products: foundProduct,
+    });
+  });
+});
 
-//Show
-app.get('/products/:id',(req, res) => {
-    Product.findById(req.params.id , (error, foundProduct) => {
-        res.render('show.ejs', {product: foundProduct})
-    })
-})
+// Update //
 
-//Update
+// Edit //
 
+app.get("/products/:id/edit", (req, res) => {
+  console.log(req.params);
+  const products1 = products.find((item) => {
+    return item.id === req.params.id;
+  });
+  res.render("edit.ejs", {
+    index: products1.id,
+    Product:products1,
+  });
+});
+// Delete //
+app.delete("/products/:id", (req, res) => {
+    Product.findByIdAndRemove(req.params.id, (error, deletedProducts) => {
+        // res.send({ success: true });
+        res.redirect('/products')
+      });
+    });
 
-
-//Delete
-
-
-
-// App Listener
+// App Listener //
 app.listen(PORT, () => console.log(`express is listening on port: ${PORT}`));
